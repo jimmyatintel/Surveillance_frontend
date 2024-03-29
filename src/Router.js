08d94824b39ac9c3fefe98f6e71f29168cd6867e
jimmy <jim1010336@gmail.com>
2024-03-29 20:33:08 +0800

    add rout to setting page

--- src/Router.js
+++ src/Router.js
@@ -9,6 +9,7 @@ import Signin from "./Pages/Signin";
 import Monitor from "./Pages/monitor";
 import Edit from "./Pages/edit";
 import Spy from "./Pages/spy";
+import Project_setting from "./Pages/Project_settting"
 import { AuthenticatedTemplate, UnauthenticatedTemplate } from '@azure/msal-react';
 
 const Router = () => (
@@ -26,18 +27,9 @@ const Router = () => (
         <Route exact path="/mapping" component={SelectLabels} />
         <Route exact path="/upload" component={Uploads} />
         <Route exact path="/spy/:project" component={Spy} />
+        <Route exact path="/setting/:project" component={Project_setting} />
       </Switch>
   </AuthenticatedTemplate>
-  {/* <Route path="/" component={ResponsiveAppBar} />
-      <Switch>
-        <Route exact path="/player/:device" component={Player} />
-        <Route exact path="/edit/:device" component={Edit} />
-        <Route exact path="/" component={Monitor} />
-        <Route exact path="/monitor" component={Monitor} />
-        <Route exact path="/mapping" component={SelectLabels} />
-        <Route exact path="/upload" component={Uploads} />
-        <Route exact path="/spy/:project" component={Spy} />
-      </Switch> */}
   </BrowserRouter>
 );
 
