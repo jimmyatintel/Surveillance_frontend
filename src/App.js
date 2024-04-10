57e31961c40e73fc0ba0f465144fdb3e2ee1060b
jimmy <jim1010336@gmail.com>
2024-04-10 13:59:59 +0800

    remove unused element

--- src/App.js
+++ src/App.js
@@ -7,15 +7,6 @@ import { MsalProvider } from "@azure/msal-react";
 import { Configuration,  PublicClientApplication } from "@azure/msal-browser";
 import { InteractionType } from "@azure/msal-browser";
 
-const msalConfiguration = {
-  auth: {
-      clientId: "8968542d-a5d2-499c-914e-de0bf20a91bd", // the only mandatory field in this object, uniquely identifies your app
-      authority: 'https://login.microsoftonline.com/46c98d88-e344-4ed4-8496-4ed7712e255d'
-      // here you'll add the other fields that you might need based on the Azure portal settings
-  }
-};
-
-const pca = new PublicClientApplication(msalConfiguration);
 
 export default function App() {
   return (
