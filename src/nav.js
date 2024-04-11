2e70ee7cc1445a0ea55c20de40784b07e94ce44f
jimmy <jim1010336@gmail.com>
2024-04-11 15:08:35 +0800

    finish adding nav status switch to http mode for develop

--- src/nav.js
+++ src/nav.js
@@ -14,21 +14,24 @@ import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import { Link } from 'react-router-dom';
 import { useMsal } from '@azure/msal-react';
+import { useHistory } from 'react-router-dom';
 
 
 const pages = ['player', 'select', 'upload'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ['Profile', 'Logout'];
 
 export default function ResponsiveAppBar() {
   const { instance, accounts } = useMsal();
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const functions = [ signOutClickHandler, '', '', signOutClickHandler];
+  const functions = [ profileClickHandler, signOutClickHandler];
   const signOutClickHandler = () => {
-    console.log("logout")
     instance.logout({
       account: accounts[0], // Specify the account to sign out, you can also pass null to sign out the current account
     });
+  }
+  const profileClickHandler = () => {
+    useHistory("/profile")
   }
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -45,141 +48,11 @@ export default function ResponsiveAppBar() {
     setAnchorElUser(null);
     // logoutRequest()
   };
-
+  React.useEffect(() => {
+    console.log(accounts[0])
+    setAnchorElUser(accounts[0].name)
+  },[]);
   return (
-    // <AppBar position="static">
-    //   <Container maxWidth="xl">
-    //     <Toolbar disableGutters>
-    //       <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-    //       <Typography
-    //         variant="h6"
-    //         noWrap
-    //         component="a"
-    //         href="/"
-    //         sx={{
-    //           mr: 2,
-    //           display: { xs: 'none', md: 'flex' },
-    //           fontFamily: 'monospace',
-    //           fontWeight: 700,
-    //           letterSpacing: '.3rem',
-    //           color: 'inherit',
-    //           textDecoration: 'none',
-    //         }}
-    //       >
-    //         LOGO
-    //       </Typography>
-
-    //       <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-    //         <IconButton
-    //           size="large"
-    //           aria-label="account of current user"
-    //           aria-controls="menu-appbar"
-    //           aria-haspopup="true"
-    //           onClick={handleOpenNavMenu}
-    //           color="inherit"
-    //         >
-    //           <MenuIcon />
-    //         </IconButton>
-    //         <Menu
-    //           id="menu-appbar"
-    //           anchorEl={anchorElNav}
-    //           anchorOrigin={{
-    //             vertical: 'bottom',
-    //             horizontal: 'left',
-    //           }}
-    //           keepMounted
-    //           transformOrigin={{
-    //             vertical: 'top',
-    //             horizontal: 'left',
-    //           }}
-    //           open={Boolean(anchorElNav)}
-    //           onClose={handleCloseNavMenu}
-    //           sx={{
-    //             display: { xs: 'block', md: 'none' },
-    //           }}
-    //         >
-    //           {pages.map((page) => (
-    //             <MenuItem key={page} onClick={handleCloseNavMenu} containerElement={<Link to="/p" />}>
-    //               <Typography textAlign="center">{page}</Typography>
-    //             </MenuItem>
-    //           ))}
-    //         </Menu>
-    //       </Box>
-    //       <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-    //       <Typography
-    //         variant="h5"
-    //         noWrap
-    //         component="a"
-    //         href=""
-    //         sx={{
-    //           mr: 2,
-    //           display: { xs: 'flex', md: 'none' },
-    //           flexGrow: 1,
-    //           fontFamily: 'monospace',
-    //           fontWeight: 700,
-    //           letterSpacing: '.3rem',
-    //           color: 'inherit',
-    //           textDecoration: 'none',
-    //         }}
-    //       >
-    //         LOGO
-    //       </Typography>
-    //       <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-    //         <Button
-    //         onClick={handleCloseNavMenu}
-    //         sx={{ my: 2, color: 'white', display: 'block' }}
-    //         href ="/monitor"
-    //         >
-    //         monitor
-    //         </Button>
-    //         <Button
-    //         onClick={handleCloseNavMenu}
-    //         sx={{ my: 2, color: 'white', display: 'block' }}
-    //         href ="/upload"
-    //         >
-    //         upload
-    //         </Button>
-    //         <Button
-    //         onClick={handleCloseNavMenu}
-    //         sx={{ my: 2, color: 'white', display: 'block' }}
-    //         href ="/mapping"
-    //         >
-    //         mapping
-    //         </Button>
-    //       </Box>
-
-    //       <Box sx={{ flexGrow: 0 }}>
-    //         <Tooltip title="Open settings">
-    //           <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-    //             <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-    //           </IconButton>
-    //         </Tooltip>
-    //         <Menu
-    //           sx={{ mt: '45px' }}
-    //           id="menu-appbar"
-    //           anchorEl={anchorElUser}
-    //           anchorOrigin={{
-    //             vertical: 'top',
-    //             horizontal: 'right',
-    //           }}
-    //           keepMounted
-    //           transformOrigin={{
-    //             vertical: 'top',
-    //             horizontal: 'right',
-    //           }}
-    //           open={Boolean(anchorElUser)}
-    //           onClose={handleCloseUserMenu}
-    //         >
-    //           {settings.map((setting,i) => (
-    //             <MenuItem key={setting} onClick={signOutClickHandler}>
-    //               <Typography textAlign="center" >{setting}</Typography>
-    //             </MenuItem>
-    //           ))}
-    //         </Menu>
-    //       </Box>
-    //     </Toolbar>
-    //   </Container>
-    // </AppBar>
     <AppBar position="static">
     <Container maxWidth="xl">
       <Toolbar disableGutters>
@@ -199,9 +72,8 @@ export default function ResponsiveAppBar() {
             textDecoration: 'none',
           }}
         >
-          LOGO
+          AI 2.0
         </Typography>
-
         <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
           <IconButton
             size="large"
@@ -255,9 +127,9 @@ export default function ResponsiveAppBar() {
             textDecoration: 'none',
           }}
         >
-          LOGO
+          AI 2.0
         </Typography>
-        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+        <Box sx={{flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
           <Button
           onClick={handleCloseNavMenu}
           sx={{ my: 2, color: 'white', display: 'block' }}
@@ -282,9 +154,9 @@ export default function ResponsiveAppBar() {
         </Box>
 
         <Box sx={{ flexGrow: 0 }}>
-          <Tooltip title="Open settings">
+          <Tooltip title={accounts[0].username}>
             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              <Avatar alt={accounts[0].name} src="/static/images/avatar/2.jpg" />
             </IconButton>
           </Tooltip>
           <Menu
@@ -304,7 +176,7 @@ export default function ResponsiveAppBar() {
             onClose={handleCloseUserMenu}
           >
             {settings.map((setting,i) => (
-              <MenuItem key={setting} onClick={signOutClickHandler}>
+              <MenuItem key={setting} onClick={functions[i]}>
                 <Typography textAlign="center" >{setting}</Typography>
               </MenuItem>
             ))}
@@ -315,4 +187,3 @@ export default function ResponsiveAppBar() {
   </AppBar>
   );
 }
-// export default ResponsiveAppBar;
